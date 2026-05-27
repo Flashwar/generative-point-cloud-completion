688c636079421ac916ac394a0a1d9144bf1b5dbe
tepper <tepper@th-brandenburg.de>
2026-05-27 21:31:51 +0200

    Add ODGNet Code

---
+++ validation_realpc_paper_results/ODGNet/extensions/chamfer_dist/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import os
+
+os.environ["TORCH_CUDA_ARCH_LIST"] = "9.0"
+setup(name='chamfer',
+      version='2.0.0',
+      ext_modules=[
+          CUDAExtension('chamfer', [
+              'chamfer_cuda.cpp',
+              'chamfer.cu',
+          ]),
+      ],
+      cmdclass={'build_ext': BuildExtension})
